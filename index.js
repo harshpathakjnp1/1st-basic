689629eefa1d284fa963b0a70a411e51c6c74d4b
Harsh Vardhan <155745954+harshpathakjnp1@users.noreply.github.com>
2024-06-17 14:37:09 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+const fs = require('fs')
+const { log, error } = require('console')
+
+app.set('view engine', 'ejs')
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static(path.join(__dirname, "public")))
+
+app.get('/', (req, res) => {
+    fs.readdir('./files', (err, files) => {
+        console.log(files)
+        res.render('index', { files: files });
+    })
+
+})
+
+app.post('/create', (req, res) => {
+    fs.writeFile(`./files/${req.body.title.split(' ').join('')}.txt`,req.body.details,(err)=>{
+        res.redirect("/")
+    })
+
+})
+
+app.listen(3000, (req, res) => {
+    
+    
+})
